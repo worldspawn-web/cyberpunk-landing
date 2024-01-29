915b317b205a8213a868ee5044510bae6653adf1
Worldspawn <worldspawn.web@gmail.com>
2024-01-29 14:58:05 +0300

    feat: icons + indexation

---
+++ frontend/src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+export { FacebookIcon } from './FacebookIcon';
+export { InstagramIcon } from './InstagramIcon';
+export { TwitchIcon } from './TwitchIcon';
+export { TwitterIcon } from './TwitterIcon';
+export { VkIcon } from './VkIcon';
+export { YoutubeIcon } from './YoutubeIcon';
