f123962db9e383fcb80da6deac1deee140ca65d0
Worldspawn <worldspawn.web@gmail.com>
2024-01-31 19:37:07 +0300

    feat: Advert component

---
+++ frontend/src/common/Advert/Advert.tsx
@@ -0,0 +1,67 @@
+import { AwesomeIcon, ColorPalette, MotionIcon } from '../../assets';
+import styles from './Advert.module.scss';
+
+export const Advert = () => {
+  return (
+    <section className={styles.advert__wrapper}>
+      <img src="../public/section_transition_bty.png" />
+      <div className={styles.advert}>
+        <div className={styles.advert__grid}>
+          <div className={styles.advert__images}>
+            <img
+              className={styles.advert__image}
+              src="../public/hp_monitor.png"
+            />
+            <img src="../public/hp_collab_cyberpunk.png" />
+          </div>
+          <div className={styles.advert__description}>
+            <h1 className={styles.advert__description__header}>
+              Полное погружение вместе с HP
+            </h1>
+            <p className={styles.advert__description__text}>
+              Погрузись в современные экшен-игры с реалистичным изображением с
+              помощью монитора с диагональю 23,8 дюйма, созданном для
+              отображения максимально насыщенных цветов. Успевай реагировать на
+              любые события с временем отклика 1 мс и частотой в 144 Гц!
+            </p>
+            <table className={styles.advert__features}>
+              <tr>
+                <td>
+                  <span className={styles.advert__features__icon}>
+                    <ColorPalette />
+                  </span>
+                </td>
+                <td className={styles.advert__features__text}>
+                  Яркие насыщенные цвета
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span className={styles.advert__features__icon}>
+                    <AwesomeIcon />
+                  </span>
+                </td>
+                <td className={styles.advert__features__text}>
+                  Кристальная четкость изображения
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span className={styles.advert__features__icon}>
+                    <MotionIcon />
+                  </span>
+                </td>
+                <td className={styles.advert__features__text}>
+                  Быстрые движения и плавный геймплей
+                </td>
+              </tr>
+            </table>
+            <div className={styles.advert__morebtn__wrapper}>
+              <a className={styles.advert__morebtn}>Подробнее</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
