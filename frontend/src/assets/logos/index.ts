f29ba643fe1bf05613cbad67f58ab646ad3ccb9a
Worldspawn <worldspawn.web@gmail.com>
2024-02-01 15:13:04 +0300

    feat: new logos

--- frontend/src/assets/logos/index.ts
+++ frontend/src/assets/logos/index.ts
@@ -1,1 +1,5 @@
 export { CyberpunkBanner } from './CyberpunkBanner';
+export { PCLogo } from './PCLogo';
+export { PS4Logo } from './PS4Logo';
+export { StadiaLogo } from './StadiaLogo';
+export { XboxOneLogo } from './XboxOneLogo';
