58f1ca3fccc6455d5fe387494c270010c00cd4ab
Worldspawn <worldspawn.web@gmail.com>
2024-02-01 15:37:29 +0300

    feat: CD Projekt Red asset

--- frontend/src/assets/logos/index.ts
+++ frontend/src/assets/logos/index.ts
@@ -3,3 +3,4 @@ export { PCLogo } from './PCLogo';
 export { PS4Logo } from './PS4Logo';
 export { StadiaLogo } from './StadiaLogo';
 export { XboxOneLogo } from './XboxOneLogo';
+export { CDProjektRed } from './CDProjektRed';
