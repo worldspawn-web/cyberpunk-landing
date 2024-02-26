33e48f02edb5d77ab0a713ab01a1ec6b6af608e9
Worldspawn <worldspawn.web@gmail.com>
2024-02-26 23:14:25 +0300

    refactor: Advert section separated with styled-components

--- frontend/src/common/Advert/Advert.tsx
+++ frontend/src/common/Advert/Advert.tsx
@@ -1,67 +1,77 @@
-import styles from './Advert.module.scss';
 import { ColorPaletteIcon, AwesomeIcon, MotionIcon } from '../../assets';
+import {
+  AdvertButton,
+  AdvertButtonWrapper,
+  AdvertDescription,
+  AdvertDiv,
+  AdvertGrid,
+  AdvertHeader,
+  AdvertIcon,
+  AdvertImage,
+  AdvertImagesWrapper,
+  AdvertTable,
+  AdvertTableCell,
+  AdvertTableRow,
+  AdvertText,
+  AdvertWrapper,
+} from './AdvertComponents';
 
 export const Advert = () => {
   return (
-    <section className={styles.advert__wrapper}>
+    <AdvertWrapper>
       <img src="../public/section_transition_bty.png" />
-      <div className={styles.advert}>
-        <div className={styles.advert__grid}>
-          <div className={styles.advert__images}>
-            <img
-              className={styles.advert__image}
-              src="../public/hp_monitor.png"
-            />
+      <AdvertDiv>
+        <AdvertGrid>
+          <AdvertImagesWrapper>
+            <AdvertImage src="../public/hp_monitor.png" />
             <img src="../public/hp_collab_cyberpunk.png" />
-          </div>
-          <div className={styles.advert__description}>
-            <h1 className={styles.advert__description__header}>
-              Полное погружение вместе с HP
-            </h1>
-            <p className={styles.advert__description__text}>
+          </AdvertImagesWrapper>
+          <AdvertDescription>
+            <AdvertHeader>Полное погружение вместе с HP</AdvertHeader>
+            <AdvertText>
               Погрузись в современные экшен-игры с реалистичным изображением с
               помощью монитора с диагональю 23,8 дюйма, созданном для
               отображения максимально насыщенных цветов. Успевай реагировать на
               любые события с временем отклика 1 мс и частотой в 144 Гц!
-            </p>
-            <table className={styles.advert__features}>
-              <tr>
-                <td>
-                  <span className={styles.advert__features__icon}>
+            </AdvertText>
+            <AdvertTable>
+              <AdvertTableRow>
+                <AdvertTableCell>
+                  <AdvertIcon>
                     <ColorPaletteIcon />
-                  </span>
-                </td>
-                <td className={styles.advert__features__text}>
-                  Яркие насыщенные цвета
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <span className={styles.advert__features__icon}>
+                  </AdvertIcon>
+                </AdvertTableCell>
+                <AdvertTableRow>
+                  <AdvertText>Яркие насыщенные цвета</AdvertText>
+                </AdvertTableRow>
+              </AdvertTableRow>
+              <AdvertTableRow>
+                <AdvertTableCell>
+                  <AdvertIcon>
                     <AwesomeIcon />
-                  </span>
-                </td>
-                <td className={styles.advert__features__text}>
-                  Кристальная четкость изображения
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <span className={styles.advert__features__icon}>
+                  </AdvertIcon>
+                </AdvertTableCell>
+                <AdvertTableCell>
+                  <AdvertText>Кристальная четкость изображения</AdvertText>
+                </AdvertTableCell>
+              </AdvertTableRow>
+              <AdvertTableRow>
+                <AdvertTableCell>
+                  <AdvertIcon>
                     <MotionIcon />
-                  </span>
-                </td>
-                <td className={styles.advert__features__text}>
+                  </AdvertIcon>
+                </AdvertTableCell>
+                <AdvertTableCell>
                   Быстрые движения и плавный геймплей
-                </td>
-              </tr>
-            </table>
-            <div className={styles.advert__morebtn__wrapper}>
-              <a className={styles.advert__morebtn}>Подробнее</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+                </AdvertTableCell>
+              </AdvertTableRow>
+            </AdvertTable>
+            <AdvertButtonWrapper>
+              <AdvertButton>Подробнее</AdvertButton>
+            </AdvertButtonWrapper>
+          </AdvertDescription>
+        </AdvertGrid>
+      </AdvertDiv>
+    </AdvertWrapper>
   );
 };
--- frontend/src/common/Advert/AdvertComponents.tsx
+++ frontend/src/common/Advert/AdvertComponents.tsx
@@ -1,79 +1,81 @@
-.advert__wrapper {
+import styled from 'styled-components';
+
+export const AdvertWrapper = styled.section`
   width: 100vw;
   height: 746px;
   background-color: #f8f200;
-}
+`;
 
-.advert {
+export const AdvertImage = styled.img`
+  margin-bottom: 26px;
+`;
+
+export const AdvertDiv = styled.div`
   max-width: 1280px;
   height: 746px;
   margin: 0 auto;
-}
+`;
 
-.advert__grid {
+export const AdvertGrid = styled.div`
   margin-top: 70px;
   display: grid;
   grid-template-columns: 50% 50%;
   grid-template-rows: 624px 624px;
-}
+`;
 
-.advert__images {
+export const AdvertImagesWrapper = styled.div`
   max-width: 430px;
   grid-row: 1;
   grid-column: 1;
   margin: 0 32px 70px 0;
-}
+`;
 
-.advert__description {
+export const AdvertDescription = styled.div`
   grid-row: 1;
   grid-column: 2;
   margin-left: 32px;
-}
+`;
 
-.advert__image {
-  margin-bottom: 26px;
-}
-
-.advert__description__header {
+export const AdvertHeader = styled.h2`
   max-width: 624px;
   font-size: 62px;
   font-family: 'Archangelsk';
   margin-bottom: 25px;
-}
+`;
 
-.advert__description__text {
+export const AdvertText = styled.p`
   max-width: 624px;
   max-height: 125px;
   font-family: 'Roboto';
   font-size: 18px;
   line-height: 34px;
-}
+`;
 
-.advert__features {
+export const AdvertTable = styled.table`
   display: flex;
   flex-direction: column;
   margin: 25px 0 40px 0;
-}
+`;
 
-.advert__features__icon {
+export const AdvertTableRow = styled.tr``;
+
+export const AdvertTableCell = styled.td``;
+
+export const AdvertIcon = styled.span`
   margin-right: 10px;
-}
+`;
 
-.advert__features__text {
-  font-size: 18px;
-}
-
-.advert__morebtn__wrapper {
+export const AdvertButtonWrapper = styled.div`
   width: 194px;
   height: 59px;
   background-color: black;
   display: flex;
   justify-content: center;
   align-items: center;
-}
+`;
 
-.advert__morebtn {
+export const AdvertButton = styled.a`
   font-family: 'Roboto';
   font-weight: bold;
   color: #f8f200;
-}
+`;
