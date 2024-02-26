fc21a0037808b445a5070f2097cda00071d81797
Worldspawn <worldspawn.web@gmail.com>
2024-02-26 23:53:04 +0300

    refactor: styled-component Lottery

--- frontend/src/common/Lottery/LotteryComponents.tsx
+++ frontend/src/common/Lottery/LotteryComponents.tsx
@@ -1,65 +1,71 @@
-a {
-  text-decoration: none;
-  cursor: pointer;
-}
+import styled from 'styled-components';
 
-.lottery__transition {
-  margin-bottom: 0;
-}
+export const LotteryChecker = styled.span`
+  transition: 0.3s all ease;
 
-.lottery__wrapper {
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const Wrapper = styled.div`
   margin-top: 0;
   background-color: black;
-}
+`;
 
-.lottery {
+export const BackgroundTransition = styled.img`
+  margin-bottom: 0;
+`;
+
+export const LotteryWrap = styled.div`
   height: 814px;
   max-width: 1280px;
   margin: 15px auto 0 auto;
-}
+`;
 
-.lottery__header__wrapper {
+export const HeaderWrap = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-}
+`;
 
-.lottery__header {
+export const SubHeader = styled.h2`
   color: white;
   font-size: 62px;
   font-family: 'Archangelsk';
   margin-left: 32px;
-}
+`;
 
-.lottery__description {
+export const Description = styled.p`
   margin-top: 25px;
   max-width: 952px;
   line-height: 34px;
   color: white;
   font-size: 18px;
   font-family: 'Roboto';
-  a {
+
+  & > a {
     color: #52bedc;
   }
-}
+`;
 
-.lottery__form__wrapper {
+export const FormWrap = styled.div`
   margin-top: 25px;
   display: grid;
   grid-template-columns: 2;
   grid-template-rows: 1;
-}
+`;
 
-.lottery__form {
+export const Form = styled.form`
   max-width: 460px;
   max-height: 383px;
   background-color: transparent;
   grid-row: 1;
   grid-column: 1;
   margin-right: 32px;
-}
+`;
 
-.lottery__form__input {
+export const FormInput = styled.input`
   color: #777777;
   background-color: transparent;
   width: 100%;
@@ -68,9 +74,9 @@ a {
   border: 0;
   border-bottom: 1px solid #777777;
   height: 35px;
-}
+`;
 
-.lottery__filedrop {
+export const Filedrop = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -81,22 +87,22 @@ a {
   border: 1px dashed #777777;
   margin: 35px 0 35px 0;
 
-  p {
+  & > p {
     color: white;
     font-family: 'Roboto';
     font-weight: 300;
   }
-}
+`;
 
-.lottery__filedrop__header {
+export const FiledropLabel = styled.p`
   font-size: 18px;
-}
+`;
 
-.lottery__filedrop__subheader {
+export const FiledropSubLabel = styled.p`
   font-size: 12px;
-}
+`;
 
-.lottery__submitbtn__wrapper {
+export const SubmitWrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -104,38 +110,30 @@ a {
   height: 59px;
   background-color: #f8f200;
   margin-bottom: 15px;
-}
+`;
 
-.lottery__submitbtn {
+export const SubmitButton = styled.a`
   color: black;
   text-decoration: none;
   font-size: 18px;
   font-family: 'Roboto';
   font-weight: 700;
-}
+`;
 
-.lottery__confirm {
+export const ConfirmChecker = styled.div`
   display: flex;
   align-items: center;
-}
+`;
 
-.lottery__confirm__text {
+export const ConfirmLabel = styled.span`
   color: white;
   font-family: 'Roboto';
   font-weight: 300;
   font-size: 12px;
   margin-left: 10px;
-}
+`;
 
-.lottery__checker {
-  transition: 0.3s all ease;
-}
-
-.lottery__checker:hover {
-  transform: scale(1.1);
-}
-
-.lottery__prize {
+export const LotteryPrize = styled.div`
   grid-row: 1;
   grid-column: 2;
-}
+`;
