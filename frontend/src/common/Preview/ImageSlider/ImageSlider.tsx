9bfd65b945ebb686b163a61ec86b1eea69831a6a
Worldspawn <worldspawn.web@gmail.com>
2024-02-27 18:39:12 +0300

    refactor: styled-component ImageSlider

--- frontend/src/common/Preview/ImageSlider/ImageSlider.tsx
+++ frontend/src/common/Preview/ImageSlider/ImageSlider.tsx
@@ -2,19 +2,21 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import classNames from 'classnames';
 
 import type { ImageSliderProps } from './ImageSlider.interface';
+import { SwiperImage } from './ImageSliderComponents';
+
 import styles from './ImageSlider.module.scss';
 
 export const ImageSlider = ({ className, ...props }: ImageSliderProps) => {
   return (
     <Swiper {...props} className={classNames(styles.swiper, className)}>
       <SwiperSlide className={styles.swiper__slide}>
-        <img className={styles.swiper__img} src="../public/main_banner_1.png" />
+        <SwiperImage src="../public/main_banner_1.png" />
       </SwiperSlide>
       <SwiperSlide className={styles.swiper__slide}>
-        <img className={styles.swiper__img} src="../public/main_banner_2.png" />
+        <SwiperImage src="../public/main_banner_2.png" />
       </SwiperSlide>
       <SwiperSlide className={styles.swiper__slide}>
-        <img className={styles.swiper__img} src="../public/main_banner_3.png" />
+        <SwiperImage src="../public/main_banner_3.png" />
       </SwiperSlide>
     </Swiper>
   );
